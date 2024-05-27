35dab067a636020a036ecd4ab7c82d32de96a277
Aslan <aslansari.dev@gmail.com>
2024-05-27 23:22:12 +0300

    feat: add change flavor and suggest antoher buttons

--- app/flavor/[flavor]/page.tsx
+++ app/flavor/[flavor]/page.tsx
@@ -1,9 +1,13 @@
+'use client'
+
 import Image from "next/image";
 import { flavors } from "@/app/lib/placeholder-data.js";
+import { useRouter } from 'next/navigation';
 
 export default function Page(
     { params }: { params: { flavor: string; } }
 ) {
+    const reload = useRouter();
     const flavor = params.flavor;
     const flavorData = flavors.find((f) => f.type === flavor)?.cocktails;
     const random: number = Math.floor(Math.random() * (flavorData?.length || 0)) || 0;
@@ -12,6 +16,7 @@ export default function Page(
         description: "No cocktails found",
         imageUrl: "/placeholder.jpg",
     };
+
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
@@ -28,6 +33,20 @@ export default function Page(
                 <div>
                     <h5 className="text-4xl font-bold pt-4">{cocktail.title}</h5>
                     <h5 className="text-base font-normal pt-4">{cocktail.description}</h5>
+                </div>
+                <div>
+                    <button className="px-4 py-2 mt-8 me-4 text-white bg-blue-500 hover:bg-blue-600 rounded-lg" >
+                        <a href="/">Change Flavor</a>
+                    </button>
+                    <button
+                        className="px-4 py-2 mt-8 text-white bg-blue-500 hover:bg-blue-600 rounded-lg"
+                        onClick={
+                            // Attempt to recover by trying to re-render the invoices route
+                            () => reload.refresh()
+                        }
+                    >
+                        <a>Suggest Another</a>
+                    </button>
                 </div>
             </div>
         </main>
